e57918031b06d233c398122da9f7de9b2ce1f9b8
Aswinvt <aswinvt010@gmail.com>
2021-04-03 15:27:59 +0530

    Added Temperature conversion and filter function0

--- pico_multicore/README.md
+++ pico_multicore/README.md
@@ -1,6 +1,10 @@
 # Raspberry Pi Pico
 ## Experimenting with Raspberry Pi Pico Multicore
+### Read temperature from internal temperature sensor of Pico
 
-> Core0 will read the ADC and PUSH the raw ADC value to memory every 100ms.
-> Core1 will POP the raw ADC value from memory and convert it into equivalent temperature value every 500ms.
-> If temperature goes beyond a predefined threshold value a Warning LED will toggel every 250ms.
+- Core0 will read the ADC and PUSH the raw ADC value to FIFO memory every 10ms.
+- Core1 will POP the raw ADC value from FIFO memory and convert it into equivalent temperature value every 500ms.
+- If temperature goes beyond a predefined threshold value a Warning LED will toggel every 250ms.
+>
+- tempcalc_fun() -  will convert the raw adc value to actual temperature value
+- filter_pt1_fun() -  will filter the temperature reading
--- pico_multicore/multicore_temp.c
+++ pico_multicore/multicore_temp.c
@@ -4,6 +4,14 @@
 #include "hardware/timer.h"
 #include "hardware/adc.h"
 
+#define serialPlotDebug 1
+#define serialPort 0
+
+// Filer function prototype
+float filter_pt1_fun(float x);
+// Temperature calculation function prototype
+float tempcalc_fun(uint16_t raw);
+
 // Core 1 Main Code
 void core1_func()
 {
@@ -17,41 +25,33 @@ void core1_func()
     // Secondary Core 1 Loop runs every 500ms
     while (1)
     {
-        // POP the raw adc value from FIFO memory
+        // POP the raw ADC value from FIFO memory
         uint16_t raw = multicore_fifo_pop_blocking();
-        // Temperature conversion
-        const float conversion_factor = 3.3f / (1 << 12);
-        float result = raw * conversion_factor;
-        float temp = 27 - (result - 0.706) / 0.001721;
-
+        // Temperature conversion function
+        float temp_pt1 = tempcalc_fun(raw);
 
         // Set the Flag for warning light
-        if (temp > 31.5)
+        if (temp_pt1 > 31)
             flag = true;
         else
             flag = false;
 
-        printf("\r\nTemperature = %f C", temp);
-        
         // Toggle the warning LED every 250ms if flag is set
         while (flag)
         {
-            // POP the raw adc value from FIFO memory
-            raw = multicore_fifo_pop_blocking();
-            // Temperature conversion
-            result = raw * conversion_factor;
-            temp = 27 - (result - 0.706) / 0.001721;
-
-            // Reset the Flag for warning light
-            if (temp < 31.5)
-                flag = false;
-                
-            printf("\r\nWarning! Temperature = %f C", temp);
-
             gpio_put(LED_PIN, 1);
             sleep_ms(250);
             gpio_put(LED_PIN, 0);
             sleep_ms(250);
+
+            // POP the raw adc value from FIFO memory
+            raw = multicore_fifo_pop_blocking();
+            // Temperature conversion function
+            float temp_pt1 = tempcalc_fun(raw);
+
+            // Reset the Flag for warning light
+            if (temp_pt1 < 32)
+                flag = false;
         }
         sleep_ms(500);
     }
@@ -78,6 +78,41 @@ int main(void)
         uint16_t raw = adc_read();
         // PUSH the ADC value to FIFO memory
         multicore_fifo_push_blocking(raw);
-        sleep_ms(100);
+        sleep_ms(10);
     }
+}
+
+float tempcalc_fun(uint16_t raw)
+{
+    // Temperature conversion
+    const float conversion_factor = 3.3f / (1 << 12);
+    float result = raw * conversion_factor;
+    float temp = 27 - (result - 0.706) / 0.001721;
+    float temp_pt1 = filter_pt1_fun(temp);
+
+#if serialPlotDebug
+    printf("%f\t%f\n", temp, temp_pt1);
+#endif
+
+#if serialPort
+    if (temp_pt1 > 32)
+        printf("\r\nWarning! Temperature = %f C", temp_pt1);
+    else
+        printf("\r\nRaw temperature = %f c \t Filtered temperature = %f c", temp, temp_pt1);
+#endif
+
+    return temp_pt1;
+}
+
+float filter_pt1_fun(float x)
+{
+    const float k = 0.30f;  // Gain
+    static float xp = 0.0f; // Previous state
+    float xi = x /* Current input */, xo = 0.0f /* Output */;
+
+    // Filter function
+    xo = xp + ((xi - xp) * k);
+    xp = xo;
+
+    return xo;
 }
